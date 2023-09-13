043b2f8090743da8e2c5e55ff20e32c4d63fb46c
tjlovegrove <aphletic@gmail.com>
2023-09-13 00:12:35 -0400

    Added JSON Serialization and Deserialization

--- Program.cs
+++ Program.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Dapper;
 using HelloWorld.Data;
 using HelloWorld.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace HelloWorld
 {
@@ -16,43 +19,91 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-            Computer myComputer = new Computer()
-            {
-                Motherboard = "TJZ997",
-                HasLTE = true,
-                HasWifi = false,
-                ReleaseDate = DateTime.Now,
-                Price = 986.45m,
-                VideoCard = "RTX 3080ti"
-            };
+            DataContextDapper dapper = new DataContextDapper(config);
 
-
-            string sql = @"INSERT INTO TutorialAppSchema.Computer (
-                Motherboard,
-                HasWifi,
-                HasLTE,
-                ReleaseDate,
-                Price,
-                VideoCard
-            ) VALUES ('" + myComputer.Motherboard
-                + "','" + myComputer.HasWifi
-                + "','" + myComputer.HasLTE
-                + "','" + myComputer.ReleaseDate
-                + "','" + myComputer.Price
-                + "','" + myComputer.VideoCard
-            + "')";
+            //string sql = @"INSERT INTO TutorialAppSchema.Computer (
+            //    Motherboard,
+            //    HasWifi,
+            //    HasLTE,
+            //    ReleaseDate,
+            //    Price,
+            //    VideoCard
+            //) VALUES ('" + myComputer.Motherboard
+            //    + "','" + myComputer.HasWifi
+            //    + "','" + myComputer.HasLTE
+            //    + "','" + myComputer.ReleaseDate
+            //    + "','" + myComputer.Price
+            //    + "','" + myComputer.VideoCard
+            //+ "')";
 
             //File.WriteAllText("log.txt", "\n" + sql + "\n";
-            using StreamWriter openFile = new("log.txt", append: true);
-            openFile.WriteLine(sql + "\n");
+            //using StreamWriter openFile = new("log.txt", append: true);
+            //openFile.WriteLine(sql + "\n");
 
-            openFile.Close();
+            //openFile.Close();
 
-            Console.WriteLine(File.ReadAllText("log.txt"));
-  
+            //Console.WriteLine(File.ReadAllText("log.txt"));
+            string computersJson = File.ReadAllText("Computers.json");
+            //Console.WriteLine(computersJson);
 
-            
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                //PropertyNameCaseInsensitive = true
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            IEnumerable<Computer>? computersNewtonSoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
+
+            IEnumerable<Computer>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
+
+
+            if (computersNewtonSoft != null)
+            {
+                foreach( Computer computer in computersNewtonSoft)
+                {
+                    //Console.WriteLine(computer.Motherboard);
+                    string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                        Motherboard,
+                        HasWifi,
+                        HasLTE,
+                        ReleaseDate,
+                        Price,
+                        VideoCard
+                    ) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
+                        + "','" + computer.HasWifi
+                        + "','" + computer.HasLTE
+                        + "','" + computer.ReleaseDate
+                        + "','" + computer.Price
+                        + "','" + EscapeSingleQuote(computer.VideoCard)
+                    + "')";
+
+                    dapper.ExecuteSql(sql);
+                }
+            }
+
+            JsonSerializerSettings settings = new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            string computersCopyNewtonSoft = JsonConvert.SerializeObject(computersNewtonSoft, settings);
+
+            File.WriteAllText("computersCopyNewtonSoft.txt", computersCopyNewtonSoft);
+
+            string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computersSystem, options);
+
+            File.WriteAllText("computersCopySystem.txt", computersCopySystem);
+
         }
+
+        static string EscapeSingleQuote(string input)
+        {
+            string output = input.Replace("'", "''");
+            return output;
+        }
+
     }
 }
